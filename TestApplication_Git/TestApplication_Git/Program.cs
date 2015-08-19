046732f689e29db3f99504331a8c3da6c2f0921c
rahulpandey87 <87.rahulpandey@gmail.com>
2015-08-19 16:53:23 -0400

    Plain Blank Project printing single line

--- TestApplication_Git/TestApplication_Git/Program.cs
+++ TestApplication_Git/TestApplication_Git/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestApplication_Git
 {
@@ -10,6 +6,7 @@ namespace TestApplication_Git
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Writing First line of code in GIT");
         }
     }
 }
